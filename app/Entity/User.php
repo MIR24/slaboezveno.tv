2550bc350e11d2f3f145b486b86f575b1bce8b59
Обловацкий Юрий <jury@observat.by>
2020-01-04 11:05:24 +0300

    Move from app/User.php to app/Entity/User.php

---
+++ app/Entity/User.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Entity;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+/**
+ * App\Entity\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Entity\User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Entity\User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Entity\User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Entity\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Entity\User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Entity\User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Entity\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Entity\User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Entity\User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Entity\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Entity\User whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
