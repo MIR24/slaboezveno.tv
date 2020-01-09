c10321526bf51fd0986c33694c2b416d2dcd18af
Обловацкий Юрий <jury@observat.by>
2020-01-09 16:37:01 +0300

    Video in one line

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -143,10 +143,6 @@
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/XsjUBfn7J84" frameborder="0"
                             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                             allowfullscreen></iframe>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col text-center">
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/7y790_Kmvz8" frameborder="0"
                             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                             allowfullscreen></iframe>
