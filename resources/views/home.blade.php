cb7deeffb21fa422d81289f90fcd66e3f7537bfd
Обловацкий Юрий <jury@observat.by>
2020-03-13 12:42:14 +0300

    Add 3 and 4 videos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -140,14 +140,18 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col text-center">
-                    <iframe width="100%" style="max-width:560px;" height="315" frameborder="0"
-                            src="https://www.youtube.com/embed/9SemO4bFlA4"
-                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                            allowfullscreen></iframe>
-                    <iframe width="100%" style="max-width:560px;" height="315"
-                            src="https://www.youtube.com/embed/0GGyAWIGbmE" frameborder="0"
-                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                            allowfullscreen></iframe>
+                    @foreach( [
+                        "https://www.youtube.com/embed/SzWMt1rqzVw",
+                        "https://www.youtube.com/embed/-X-_TZbSWz0",
+                        "https://www.youtube.com/embed/9SemO4bFlA4",
+                        "https://www.youtube.com/embed/0GGyAWIGbmE"
+                    ] as $iframeSrc )
+                        <iframe width="100%" style="max-width:560px;" height="315" frameborder="0"
+                                src="{{ $iframeSrc }}"
+                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                allowfullscreen></iframe>
+
+                    @endforeach
                 </div>
             </div>
         </div>
